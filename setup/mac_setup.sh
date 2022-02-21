9ebcc10d2657a9df3f2614376b7e2f95aff14e03
jeronimobarea <35541010+jeronimobarea@users.noreply.github.com>
2022-02-21 12:20:58 +0100

    add fast macos setup

---
+++ setup/mac_setup.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask alacritty
+brew install tmux
+brew install neovim
+brew install docker
