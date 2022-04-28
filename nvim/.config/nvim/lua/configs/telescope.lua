2bf42bdd7c4a7598a29e3b1ac13bb3240bb77752
jeronimo.lucas <jeronimo.lucas@thunes.com>
2022-04-28 15:43:25 +0200

    feat: improve telescope config

--- nvim/.config/nvim/lua/configs/telescope.lua
+++ nvim/.config/nvim/lua/configs/telescope.lua
@@ -8,8 +8,17 @@ telescope.setup {
     },
     defaults = {
         file_ignore_patterns = {
-            ".git"
+            ".git",
+            "go.sum",
         }
-    }
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        },
+    },
 }
+
+require("telescope").load_extension("fzy_native")
 
