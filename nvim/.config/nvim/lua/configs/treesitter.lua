186fed79ca73f421197521376d71e0b993cda82c
jeronimo.lucas <jeronimo.lucas@thunes.com>
2022-04-29 18:24:45 +0200

    feat: add json and yaml as default in treesitter

--- nvim/.config/nvim/lua/configs/treesitter.lua
+++ nvim/.config/nvim/lua/configs/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "lua", "go" },
+  ensure_installed = { "lua", "go", "json", "yaml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
