5155964728f11dadc38e9de761192b6342a9d54b
jeronimo.lucas <jeronimo.lucas@thunes.com>
2022-05-13 19:34:07 +0200

    feat: use function for remaps reducing verbosity

--- nvim/.config/nvim/lua/configs/init.lua
+++ nvim/.config/nvim/lua/configs/init.lua
@@ -1,3 +1,19 @@
+function CreateNoremapGlobal(type, opts)
+    return function (lhs, rhs)
+        vim.api.nvim_set_keymap(type, lhs, rhs, opts)
+    end
+end
+
+function CreateNoremap(type, opts)
+    return function(lhs, rhs, bufnr)
+        bufnr = bufnr or 0
+        vim.api.nvim_buf_set_keymap(bufnr, type, lhs, rhs, opts)
+    end
+end
+
+NnoremapGlobal = CreateNoremapGlobal("n", { noremap = true })
+Nnoremap = CreateNoremap("n", { noremap = true })
+
 require("configs.treesitter")
 require("configs.airline")
 require("configs.telescope")
--- nvim/.config/nvim/lua/configs/lsp.lua
+++ nvim/.config/nvim/lua/configs/lsp.lua
@@ -33,12 +33,10 @@ cmp.setup({
 })
 
 -- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+NnoremapGlobal('<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+NnoremapGlobal('[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+NnoremapGlobal(']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+NnoremapGlobal('<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -48,19 +46,19 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  Nnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+  Nnoremap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+  Nnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+  Nnoremap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  Nnoremap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  Nnoremap('<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+  Nnoremap('<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+  Nnoremap('<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+  Nnoremap('<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  Nnoremap('<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  Nnoremap('<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+  Nnoremap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  Nnoremap('<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 end
 
 local servers = {'pyright', 'rust_analyzer', 'gopls', 'sumneko_lua'}
