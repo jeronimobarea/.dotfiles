9985888d3d12690ae4301d75867ff24a1cb12ee3
Jeronimo Barea Lucas <jeronimobarea@Jeronimos-MacBook-Pro.local>
2022-05-22 17:25:20 +0200

    feat(nvim): remove treesitter unnecessary code

--- nvim/.config/nvim/lua/configs/treesitter.lua
+++ nvim/.config/nvim/lua/configs/treesitter.lua
@@ -1,12 +1,10 @@
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
     ensure_installed = { "lua", "go", "rust", "python", "json", "yaml" },
     sync_install = false,
     indent = {
         enable = true,
     },
     highlight = {
-        -- `false` will disable the whole extension
         enable = true,
     },
     incremental_selection = {
