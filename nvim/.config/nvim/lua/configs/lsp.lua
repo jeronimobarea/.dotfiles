36b37813b1edbcfab1ef50475a0c8ed71247a183
jeronimo.lucas <jeronimo.lucas@thunes.com>
2022-10-17 12:56:03 +0200

    fix: update deprecated function call

--- nvim/.config/nvim/lua/configs/lsp.lua
+++ nvim/.config/nvim/lua/configs/lsp.lua
@@ -12,7 +12,7 @@ local source_mapping = {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspkind = require("lspkind")
