bdff42d493219509dad3d14b50bed6ff47a7a514
Jeronimo Barea Lucas <jeronimobarea@Jeronimos-MacBook-Pro.local>
2023-06-27 19:08:23 +0200

    feat(nvim): add lualine tab bar

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -47,4 +47,18 @@ require("lualine").setup({
         },
         lualine_x = { "encoding" },
     },
+    tabline = {
+        lualine_a = {
+            {
+                "tabs",
+                -- Note:
+                -- It can also be a function that returns
+                -- the value of `max_length` dynamically.
+                mode = 2, -- 0: Shows tab_nr
+                -- 1: Shows tab_name
+                -- 2: Shows tab_nr + tab_name
+
+            },
+        },
+    },
 })
