23fa2e0cfa601952ffb8a23ba400a31fe87ef6c5
jeronimo.lucas <jeronimo.lucas@thunes.com>
2023-09-11 18:24:21 +0200

    fix(nvim): go nvim import not working properly

--- nvim/.config/nvim/after/plugin/go_nvim.lua
+++ nvim/.config/nvim/after/plugin/go_nvim.lua
@@ -1,13 +1,3 @@
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-        require("go.format").goimport()
-    end,
-    group = format_sync_grp,
-})
-
 if not pcall(require, "go") then
     return
 end
